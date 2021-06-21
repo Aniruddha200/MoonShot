05c0b0a2a2995b360c0f7b51207cec6a9baeb5eb
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-21 11:16:56 +0100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by administrator on 21/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
